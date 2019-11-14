7058dbc3356cc1ee277196eea74b625b25d89f0e
AMForan <becool_346@hotmail.com>
2019-11-14 18:13:38 -0500

    First Commit
    Basic HTML Layout, Basic CSS Layout, no working functions in JS

---
+++ cardEvaluation.js
@@ -0,0 +1,19 @@
+//Declaring Variables
+
+
+var firstCard = "", 
+secondCard = "", 
+firstNumber = "", 
+secondNumber = "", 
+firstSuit = "", 
+secondSuit = "",  
+potSize = 0,
+potOdds = 0,
+cardEquity = 0,
+numPlayers = 0;
+position = "";
+
+
+function cardEvaluation(firstCard, secondCard, potSize,numPlayers, position){
+
+}	
